f726128f0391bbf1d3adeed260ef1cb1057bee9f
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-06-14 20:49:21 +0100

    Problems using fold, where and map in Dart

--- list.dart
+++ list.dart
@@ -4,7 +4,7 @@
 // var fruits = <String> ["Mango", "Orange"];
 
 void main(){
-  var fruits = ["Mango", "Apple", "Banana"];
+  // var fruits = ["Mango", "Apple", "Banana"];
   // print(fruits[0]);
 
   // fruits[1] = "Watermelon";
@@ -19,7 +19,26 @@ void main(){
   // fruits.removeAt(fruits.length-1);
   // print(fruits);
 
-  
+  // Iteration over Lists
+  // for(var i=0; i < fruits.length; i++){
+  //   print(fruits[i]);
+  // }  
+
+  // for(var fruit in fruits){
+  //   print(fruit);
+  // }
+
+  // fruits.forEach((fruit)=>print(fruit));
+
+  var numbers = [1,2,3,4,5];
+  var squared = numbers.map((number)=>number * number).toList();
+  print(squared);
+
+  var evenOnly = numbers.where((number)=>number.isEven).toList();
+  print(evenOnly);
+
+  var total = numbers.reduce((initial, curval)=>initial + curval);
+  print(total);
 
 }
 
